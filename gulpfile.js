5d646dd007238b568c2fa92b35566e97948feac9
marcoantonioghiani01 <marcoantonio.ghiani01@gmail.com>
2019-05-21 12:49:07 +0200

    upgrade to v1.1.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const fs = require('fs');
@@ -9,6 +8,7 @@ const $ = require('gulp-load-plugins')();
 // when generating the glob patterns array for gulp.src()
 function addDefSrcIgnore (srcArr) {
   return srcArr.concat([
+    '!**/REMOVE{,/**}',
     '!node_modules{,/**}',
     '!private{,/**}',
     '!dist{,/**}',
@@ -29,62 +29,59 @@ gulp.task('default', function () {
   ]).on('change', browserSync.reload);
 });
 
-// Lint all files
-gulp.task('lint', ['lint-js', 'lint-html', 'lint-css']);
-
 // JavaScript and JSON linter
-gulp.task('lint-js', function () {
+function lintJs () {
   return gulp.src(addDefSrcIgnore(['**/*.js', '**/*.json']), {dot: true})
     .pipe($.eslint({dotfiles: true}))
     .pipe($.eslint.format())
     .pipe($.eslint.failAfterError());
-});
+}
 
 // HTML linter
-gulp.task('lint-html', function () {
+function lintHtml () {
   return gulp.src(addDefSrcIgnore(['**/*.html']))
-    .pipe($.htmlLint({htmllintrc: '.htmllintrc.json'}))
-    .pipe($.htmlLint.format())
-    .pipe($.htmlLint.failAfterError());
-});
+    .pipe($.htmllint({config: '.htmllintrc.json', failOnError: true}));
+}
 
 // CSS linter
-gulp.task('lint-css', function () {
+function lintCss () {
   return gulp.src(addDefSrcIgnore(['**/*.css']))
     .pipe($.stylelint({
       failAfterError: true,
-      reporters: [
-        {formatter: 'string', console: true}
-      ]
+      reporters: [{formatter: 'string', console: true}]
     }));
-});
+}
 
 // Remove solutions from exercises
-gulp.task('remove-solutions', ['lint'], function () {
+function removeSolutions () {
   del.sync('dist');
   return gulp.src(addDefSrcIgnore(['**']), {dot: true})
     .pipe($.replace(/^\s*(\/\/|<!--|\/\*)\s*REMOVE-START[\s\S]*?REMOVE-END\s*(\*\/|-->)?\s*$/gm, ''))
     .pipe(gulp.dest('dist'));
-});
+}
 
 // Prepare for distribution to students
-gulp.task('dist', ['remove-solutions'], function () {
-
-  function removeMaster (str) {
-    var strArr = str.split('-');
-    strArr[strArr.length - 1] === 'master' && strArr.pop();
-    return strArr.join('-');
-  }
-
-  const npmConfig = require('./package.json');
-  npmConfig.name = removeMaster(npmConfig.name);
-  npmConfig.repository.url = removeMaster(npmConfig.repository.url);
-  npmConfig.scripts['precommit'] = 'gulp lint';
-  fs.writeFileSync('dist/package.json', JSON.stringify(npmConfig, null, 2));
-
+function updateConfigForSlave (done) {
+  let npmConfig = require('./package.json');
+  npmConfig = JSON.stringify(npmConfig, null, 2).replace(/-master/g, '');
+  fs.writeFileSync('dist/package.json', npmConfig);
   const esLintConfig = require('./.eslintrc.json');
   esLintConfig.rules['no-undef'] = 'off';
   esLintConfig.rules['no-unused-vars'] = 'off';
   fs.writeFileSync('dist/.eslintrc.json', JSON.stringify(esLintConfig, null, 2));
 
-});
+  done();
+}
+
+// Lint all files
+exports.lint = gulp.parallel(
+  lintJs,
+  lintHtml,
+  lintCss
+);
+
+// Prepare for distribution to students
+exports.dist = gulp.series(
+  removeSolutions,
+  updateConfigForSlave
+);
